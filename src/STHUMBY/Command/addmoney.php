c78c8163b63fb6421bf480ac26cb7141c69eb6cd
STHUMBY <damien.thiry47@gmail.com>
2021-12-13 19:41:07 +0100

    No Argument Crash fixed

--- src/STHUMBY/Command/addmoney.php
+++ src/STHUMBY/Command/addmoney.php
@@ -19,29 +19,25 @@ class addmoney extends Command
     }
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        if ($sender->hasPermission('economy.addmoney') || $sender instanceof ConsoleCommandSender  || $sender->getName() === "STHUMBY"){
-            if (is_null($args[0])){
-                $sender->sendMessage("Invalid argument");
-                return;
-            }
-            if (is_null($args[1])){
-                $sender->sendMessage("Invalid argument");
-                return;
-            }
-            $player = Server::getInstance()->getPlayerExact($args[0]);
-            if (isset($player)){
-                EconomyManager::addMoney($player, $args[1]);
-                $sender->sendMessage($args[1] . " money has been added to " . $player->getName());
-                if ($sender instanceof Player){
-                    $player->sendMessage($sender->getName() . " add " . $args[1] . "$ to your bank");
-                }else{
-                    $player->sendMessage("Server has added to you " . $args[1] . "$");
+        if (!is_null($args[0]) && !is_null($args[1]) && is_int($args[1])) {
+            if ($sender->hasPermission('economy.addmoney') || $sender instanceof ConsoleCommandSender || $sender->getName() === "STHUMBY") {
+                $player = Server::getInstance()->getPlayerExact($args[0]);
+                if (isset($player)) {
+                    EconomyManager::addMoney($player, $args[1]);
+                    $sender->sendMessage($args[1] . " money has been added to " . $player->getName());
+                    if ($sender instanceof Player) {
+                        $player->sendMessage($sender->getName() . " add " . $args[1] . "$ to your bank");
+                    } else {
+                        $player->sendMessage("Server has added to you " . $args[1] . "$");
+                    }
+                } else {
+                    $sender->sendMessage("Player not found");
                 }
-            }else{
-                $sender->sendMessage("Player not found");
+            } else {
+                $sender->sendMessage(TextFormat::RED . "You have not the permission to do this command");
             }
         }else{
-            $sender->sendMessage(TextFormat::RED . "You have not the permission to do this command");
+            $sender->sendMessage("Arguments invalid");
         }
     }
 }
--- src/STHUMBY/Command/removemoney.php
+++ src/STHUMBY/Command/removemoney.php
@@ -14,21 +14,15 @@ class removemoney extends Command{
     }
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        if (is_null($args[0])){
-            $sender->sendMessage("Invalid argument");
-            return;
-        }
-        if (is_null($args[1])){
-            $sender->sendMessage("Invalid argument");
-            return;
-        }
-        if ($sender->hasPermission("economy.money.remove") || $sender instanceof ConsoleCommandSender || $sender->getName() === "STHUMBY"){
-            $player = Server::getInstance()->getPlayerExact($args[0]);
-            if (isset($player)){
-                EconomyManager::removeMoney($player, $args[1]);
-                $sender->sendMessage($args[1] . " money has been removed from " . $player->getName());
-            }else{
-                $sender->sendMessage("Can't found the player : " . $args[0]);
+        if (!is_null($args[0])&& !is_null($args[1])&& is_int($args[1])) {
+            if ($sender->hasPermission("economy.money.remove") || $sender instanceof ConsoleCommandSender || $sender->getName() === "STHUMBY") {
+                $player = Server::getInstance()->getPlayerExact($args[0]);
+                if (isset($player)) {
+                    EconomyManager::removeMoney($player, $args[1]);
+                    $sender->sendMessage($args[1] . " money has been removed from " . $player->getName());
+                } else {
+                    $sender->sendMessage("Can't found the player : " . $args[0]);
+                }
             }
         }
     }
